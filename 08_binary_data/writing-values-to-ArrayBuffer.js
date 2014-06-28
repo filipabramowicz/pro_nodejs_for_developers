d6f6cb03670cf332174cb31db254e293f2d1f86b
filipabramowicz <filip.abramowicz@gmail.com>
2014-06-29 00:41:37 +0200

    ArrayBuffer creation and printing

---
+++ 08_binary_data/writing-values-to-ArrayBuffer.js
@@ -0,0 +1,13 @@
+var foo = new ArrayBuffer(4);
+
+foo[0] = 0;
+foo[1] = 1;
+foo[2] = 2;
+foo[3] = 3;
+// this assignment will fail silently
+foo[4] = 4;
+console.log(foo);
+
+for (var i = 0, len = foo.byteLength; i < len; i++) {
+	console.log(foo[i]);
+}
