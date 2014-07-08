3ac6658381a062296b1079862264d3f877bd1a34
filipabramowicz <filip.abramowicz@gmail.com>
2014-07-08 22:08:49 +0200

    Minor comment update

--- 11_http/providing-different-response-codes.js
+++ 11_http/providing-different-response-codes.js
@@ -1,5 +1,6 @@
 var http = require('http');
 http.createServer(function(request, response){
+	// successful reponse (200 OK) only for /foo request
 	if (request.url === "/foo") {
 		response.end("Hello <strong>HTTP</strong>");
 	} else {
