166e0946cebdfeecb3899284bb4ef77aea850d6e
OpenShift guest <filip.abramowicz@gmail.com>
2014-07-21 11:03:17 -0400

    Additional description for send method

--- 12_the_express_framework/supporting-multiple-resources-using-express-framework.js
+++ 12_the_express_framework/supporting-multiple-resources-using-express-framework.js
@@ -4,6 +4,13 @@ var app = express();
 
 // there are simmilar methods for put, post and delete
 app.get("/", function(req, res, next){
+    // If first argument is a number, then it is treated as status code
+    // If not specified then 200 is sent
+    // Content-Type is also set dependign on the type fo the object
+    // If response body is a buffer then content-type is set to application/octet-stream
+    // If response body is a string then content-type is set to text/html
+    // If response body is array or object then content-type is set to JSON
+    // If not body is provided then status code's reason phrase is used
 	res.send("Hello <strong>home page</strong>");
 });
 
