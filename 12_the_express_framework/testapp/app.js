83da8448c93463c01bd074b26427c2f5044d9948
OpenShift guest <filip.abramowicz@gmail.com>
2014-07-23 06:17:54 -0400

    New view and express-validator used

--- 12_the_express_framework/testapp/app.js
+++ 12_the_express_framework/testapp/app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes'),
+    validator = require("express-validator");
 
 var app = module.exports = express.createServer();
 
@@ -17,6 +18,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(validator());
 });
 
 app.configure('development', function(){
@@ -30,6 +32,38 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+
+app.get("/products/:productId", function(req, res, next) {
+    var errors;
+    
+    // Assert takes parameter name and error message as arguments
+    // Validation methods:
+    // - notEmpty()
+    // - isAlpha()
+    // - len()
+    // There are also:
+    // - isInt()
+    // - isEmail()
+    // - isNull()
+    // - is()
+    // - contains()
+    req.assert("productId", "Missing product ID").notEmpty();
+    req.assert("productId", "Invalid product ID").isAlpha().len(2, 10);
+    errors = req.validationErrors();
+    
+    // There is also sanitize() method:
+    // req.sanitize("parameter").toBoolean() - changes to boolean
+    // req.sanitize("parameter").toInt() - changes to integer
+    // req.sanitize("parameter").trim() - trims spaces before and after parameter
+    // req.sanitize("parameter").entityDecode() - replaces character entities (ie &lt and &gt) with corresponding characters (< and >)
+    
+    if (errors) {
+        return res.send(errors);
+    }
+    
+    res.send("Requested " + req.params.productId);
+});
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
